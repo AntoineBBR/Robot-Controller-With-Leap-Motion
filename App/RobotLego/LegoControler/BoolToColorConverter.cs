bca465d22e4dcbf65b5e1afcce33af0dd134d849
vaclergue <valentin.clergue@etu.uca.fr>
2022-02-03 12:00:13 +0100

    Ajout d'un module pour discuter avec un robot lego, la connexion à ce dernier ne marche plus pour une raison inconnue

---
+++ App/RobotLego/LegoControler/BoolToColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LegoControler
+{
+    class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool buttonPressed = (bool)value;
+            return buttonPressed ? new SolidColorBrush(Colors.LightSeaGreen) : new SolidColorBrush(Colors.Gray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ App/RobotLego/LegoControler/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using AsyncEV3MotorCommandsLib;
+using BluetoothDevicesScanner;
+using Lego.Ev3.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LegoControler
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BluetoothDevice selectedDevice;
+        event EventHandler DeviceSelected;
+
+        BrickManager brickManager = new BrickManager();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = brickManager;
+        }
+
+        internal static Dictionary<char, OutputPort> ports = new Dictionary<char, OutputPort>()
+        {
+            {'A', OutputPort.A},
+            {'B', OutputPort.B},
+            {'C', OutputPort.C},
+            {'D', OutputPort.D}
+        };
+
+        internal static Dictionary<char, InputPort> inputPorts = new Dictionary<char, InputPort>()
+        {
+            {'A', InputPort.A},
+            {'B', InputPort.B},
+            {'C', InputPort.C},
+            {'D', InputPort.D}
+        };
+
+        public BluetoothDevice SelectedDevice
+        {
+            get { return selectedDevice; }
+            set
+            {
+                selectedDevice = value;
+                if (value != null)
+                {
+                    if (brickManager.Connected)
+                        brickManager.Disconnect();
+                    DeviceSelected?.Invoke(this, null);
+                }
+            }
+        }
+
+        private void root_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (brickManager.Connected)
+                brickManager.Disconnect();
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button)) return;
+            char port = ((sender as Button).Content as string).Last();
+            if (!ports.ContainsKey(port)) return;
+
+            //await brickManager.Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(ports[port], 100, 2000, true);
+            
+            await brickManager.Brick.DirectCommand.TurnMotorAtPowerAsync(ports[port], 100);
+            await Task.Delay(1000);
+            //await brickManager.DirectCommand.StopMotorAsync(inputPorts[port]);
+            //await Task.Delay(1000);
+            //await brickManager.Brick.DirectCommand.TurnMotorAtPowerAsync(ports[port], -100);
+            //await Task.Delay(2000);
+            //await brickManager.DirectCommand.StopMotorAsync(inputPorts[port]);
+        }
+
+        private void root_Loaded(object sender, RoutedEventArgs e)
+        {
+            DeviceSelected += MainWindow_DeviceSelected;
+        }
+
+        private async void MainWindow_DeviceSelected(object sender, EventArgs e)
+        {
+            await brickManager.ConnectAsync(SelectedDevice?.DeviceName, SelectedDevice?.COMPort);
+
+            if (brickManager.Connected)
+            {
+                // Le son est affreux, mes oreilles SVP
+                //await brickManager.PlayThirdKindAsync(volume: 10, duration: 250);
+            }
+        }
+    }
+}
