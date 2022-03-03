8487de33f8f04e4a5f314415760962890e6ce807
Antoine <antoine.imbert@etu.uca.fr>
2022-03-03 10:49:04 +0100

    gestion des CLONFLITS

--- App/RobotLego/LegoController/Manager.cs
+++ App/RobotLego/LegoController/Manager.cs
@@ -4,13 +4,8 @@ namespace LegoController
 {
     public class Manager
     {
-<<<<<<< HEAD
         public static ManagerController managerCtrl = new ManagerController();
-=======
->>>>>>> 054265cd457a8786696794c2b579d7025475c9c7
         public static BrickManager brickManager = new BrickManager();
         public static Commands commands = new Commands();
-
-        //public static ManagerController managerCtrl = new ManagerController(false);
     }
 }
